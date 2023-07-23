af7ea84f19aa034d8ef8e5b1195974aeff0770ee
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-07-23 01:54:49 +0100

    update _printf function, it now will print single characters

--- _printf.c
+++ _printf.c
@@ -5,12 +5,22 @@
  * @format: character string that contains directives of how to print
  * Return: 0 on success
  */
-
 int _printf(const char *format, ...)
 {
 	int i;
+	va_list args;
+
+	va_start(args, format);
 
 	for (i = 0; *(format + i) != '\0'; i++)
-		write(1, format + i, 1);
+	{
+		if (*(format + i) == '%')
+		{
+			converter(format + ++i)(args);
+		} else
+		{
+			write(1, format + i, 1);
+		}
+	}
 	return (0);
 }
