979912bc193f51c519ca9fe932b25b5d10ae090b
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-07-24 20:22:09 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,26 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
+
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
 int _printf(const char *format, ...);
 typedef struct formatter {
 	char * symbol;
