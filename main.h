59b6f680be7025fd201aa89930998d20acbeca2e
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-07-24 22:46:46 +0100

    Update main.h add structures buffer_s converter_s and flag_s

--- main.h
+++ main.h
@@ -21,14 +21,39 @@
 /* Length Modifier Macros */
 #define SHORT 1
 #define LONG 2
+
+typedef struct buffer_s
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
+
+/**
+ * struct converter_s - A new type defining a converter struct.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+} converter_t;
+
+/**
+ * struct flag_s - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
+ */
+typedef struct flag_s
+{
+	unsigned char flag;
+	unsigned char value;
+} flag_t;
+
 int _printf(const char *format, ...);
-typedef struct formatter {
-	char * symbol;
-	void (*f)(char *);
-} format_me;
-void (*converter(char *))(va_list list);
-void print_char(va_list list);
-void print_string(va_list list);
-void print_d(va_list list);
-void print_i(va_list list);
+
+buffer_t *init_buffer(void);
+
 #endif
