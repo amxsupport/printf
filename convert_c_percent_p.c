6eeb28b9a264190437ca07ccb5f9940ccb8e4e3e
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-07-25 23:47:30 +0100

    Update convert_c_percent_p.c implementation convert_p

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -86,5 +86,20 @@ unsigned int convert_percent(va_list args, buffer_t *output,
 unsigned int convert_p(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-/* implementation convert_p */
+	char *null = "(nil)";
+	unsigned long int address;
+	unsigned int ret = 0;
+
+	(void)len;
+
+	address = va_arg(args, unsigned long int);
+	if (address == '\0')
+		return (_memcpy(output, null, 5));
+
+	flags |= 32;
+	ret += convert_ubase(output, address, "0123456789abcdef",
+			flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
