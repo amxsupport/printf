893113a736da6d29f0733d9a8ece8c7b48b2b2e3
amxsupport <itmasterdesigne@gmail.com>
2023-07-25 13:10:14 +0000

    add handlers for implementation

--- handlers.c
+++ handlers.c
@@ -6,8 +6,69 @@
 
 #include "main.h"
 
+unsigned char handle_flags(const char *flag, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char);
+
+/**
+ * handle_flags - Matches flags with corresponding values.
+ * @flag: A pointer to a potential string of flags.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If flag characters are matched - a corresponding value.
+ *         Otherwise - 0.
+ */
+unsigned char handle_flags(const char *flag, char *index)
+{
+	/* implement handel_flags */
+}
+
+/**
+ * handle_length - Matches length modifiers with their corresponding value.
+ * @modifier: A pointer to a potential length modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a lenth modifier is matched - its corresponding value.
+ *         Otherwise - 0.
+ */
+unsigned char handle_length(const char *modifier, char *index)
+{
+	/* implement hande_length */
+}
+
+/**
+ * handle_width - Matches a width modifier with its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential width modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a width modifier is matched - its value.
+ *         Otherwise - 0.
+ */
+int handle_width(va_list args, const char *modifier, char *index)
+{
+	/* implement hande_wigth */
+}
+
+/**
+ * handle_precision - Matches a precision modifier with
+ *                    its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential precision modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a precision modifier is matched - its value.
+ *         If the precision modifier is empty, zero, or negative - 0.
+ *         Otherwise - -1.
+ */
+int handle_precision(va_list args, const char *modifier, char *index)
+{
+	/* implement hande_precision */
+}
+
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
@@ -15,7 +76,7 @@ unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
  *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
- */
+*/
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char)
 {
