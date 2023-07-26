b69626a9850cf5bbd347e2cff745fdc08f9f1d6f
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-07-26 01:36:29 +0100

    Update convert_numbers.c implementation of convert_di and convert_b

--- convert_numbers.c
+++ convert_numbers.c
@@ -29,7 +29,61 @@ unsigned int convert_o(va_list args, buffer_t *output,
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-  /* implementation of convert_di */
+	long int d, copy;
+	unsigned int ret = 0, count = 0;
+	char pad, space = ' ', neg = '-', plus = '+';
+
+	if (len == LONG)
+		d = va_arg(args, long int);
+	else
+		d = va_arg(args, int);
+	if (len == SHORT)
+		d = (short)d;
+
+	/* Handle space flag */
+	if (SPACE_FLAG == 1 && d >= 0)
+		ret += _memcpy(output, &space, 1);
+
+	if (prec <= 0 && NEG_FLAG == 0) /* Handle width  */
+	{
+		if (d == LONG_MIN)
+			count += 19;
+		else
+		{
+			for (copy = (d < 0) ? -d : d; copy > 0; copy /= 10)
+				count++;
+		}
+		count += (d == 0) ? 1 : 0;
+		count += (d < 0) ? 1 : 0;
+		count += (PLUS_FLAG == 1 && d >= 0) ? 1 : 0;
+		count += (SPACE_FLAG == 1 && d >= 0) ? 1 : 0;
+
+		/* Handle plus flag when zero flag is active */
+		if (ZERO_FLAG == 1 && PLUS_FLAG == 1 && d >= 0)
+			ret += _memcpy(output, &plus, 1);
+		/*Print negative sign when zero flag is active */
+		if (ZERO_FLAG == 1 && d < 0)
+			ret += _memcpy(output, &neg, 1);
+
+		pad = (ZERO_FLAG == 1) ? '0' : ' ';
+		for (wid -= count; wid > 0; wid--)
+			ret += _memcpy(output, &pad, 1);
+	}
+
+	/* Print negative sign when zero flag is not active */
+	if (ZERO_FLAG == 0 && d < 0)
+		ret += _memcpy(output, &neg, 1);
+	/* Handle plus flag when zero flag is not active */
+	if (ZERO_FLAG == 0 && (PLUS_FLAG == 1 && d >= 0))
+		ret += _memcpy(output, &plus, 1);
+
+	if (!(d == 0 && prec == 0))
+		ret += convert_sbase(output, d, "0123456789",
+				flags, 0, prec);
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
 
 /**
@@ -47,7 +101,13 @@ unsigned int convert_di(va_list args, buffer_t *output,
 unsigned int convert_b(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-  /* implementation of convert_b */
+	unsigned int num;
+
+	num = va_arg(args, unsigned int);
+
+	(void)len;
+
+	return (convert_ubase(output, num, "01", flags, wid, prec));
 }
 
 /**
