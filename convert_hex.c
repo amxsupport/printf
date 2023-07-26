b705c025123350851b11fb58d889e371570dc7b3
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-07-26 01:14:03 +0100

    update convert_hex.c implementation of convert_x, convert_X

--- convert_hex.c
+++ convert_hex.c
@@ -24,7 +24,27 @@ unsigned int convert_X(va_list args, buffer_t *output,
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-/* implementation convert_x */
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0x";
+
+	if (len == LONG)
+		num = va_arg(args, unsigned long int);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+
+	if (!(num == 0 && prec == 0))
+		ret += convert_ubase(output, num, "0123456789abcdef",
+				flags, wid, prec);
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
 
 /**
@@ -42,5 +62,25 @@ unsigned int convert_x(va_list args, buffer_t *output,
 unsigned int convert_X(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-/* implementation convert_X */
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0X";
+
+	if (len == LONG)
+		num = va_arg(args, unsigned long);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+
+	if (!(num == 0 && prec == 0))
+		ret += convert_ubase(output, num, "0123456789ABCDEF",
+				flags, wid, prec);
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
